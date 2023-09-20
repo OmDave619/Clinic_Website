9b0f364f70e8369e48fd428a98f60f236ea5df48
OmDave619 <odave420@gmail.com>
2023-09-20 22:10:45 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let element = $('.title h2:last');
+
+    let texts = ['Healing Holistic Way','Yoga','Mind','Body','Soul','Diet','Homeopathy']
+    let currentindex=0;
+
+    function typeText() {
+        element.text(texts[currentindex]);
+        element.fadeIn(2000);
+        setTimeout(eraseText,2000);
+        
+    }
+
+    function eraseText() {
+        element.fadeOut(2000)
+        currentindex = (currentindex+1) % texts.length;
+        setTimeout(typeText,2000);
+    }
+
+typeText()
+
